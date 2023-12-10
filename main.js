78f6ea1862df527c92234f2cc4228a2a571987a6
RC908 <138607520+RC908@users.noreply.github.com>
2023-12-10 09:01:27 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name = "Ranbir ";
+var last_name = "Kapoor";
+var fullname = first_name.concat(last_name);
+console.log(fullname);
+
+var a = 10;
+var b = 5;
+var c = a - b;
+console.log(c);
